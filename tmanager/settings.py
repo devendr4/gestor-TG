51e782c931f7f6354673eaef76f31915f8956d9c
spectrox0 <alejanvelazco2008@hotmail.com>
2020-08-18 15:45:36 -0400

    sa

--- tmanager/settings.py
+++ tmanager/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -143,3 +143,5 @@ LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
 LOGIN_REDIRECT_URL = 'proposals:proposals_list'
 LOGOUT_REDIRECT_URL = 'home'
+
+django_heroku.settings(locals())
